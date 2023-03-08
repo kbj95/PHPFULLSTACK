5ee319a9870e163433befdefffd75ffdf90f0704
kbj95 <gwonbongjeong@gmail.com>
2023-03-08 18:04:06 +0900

    23-03-08

---
+++ CSS/Example/js/script1.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".mainMenu > li").mouseover(function(){
+        $(".subMenu").stop().slideDown(200);
+    });
+    $(".mainMenu li").mouseout(function(){
+        $(".subMenu").stop().slideUp(200);
+    });
+
+    var now = 0;
+
+    start();
+    function start(){
+        $("slide").eq(0).siblings().css("margin-top","-500px")
+        setInterval(function(){slide();},3000)
+    }
+    function slide(){
+        now = now == 2 ? 0 : now+= 1;
+        $('.slide').eq(now-1).css('margin-top','-500px');
+        $('.slide').eq(now).css('margin-top','0');
+    }
+});
